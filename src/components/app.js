f29920e9ae1be194af6a968f0f3089cfac7e9445
Cameron KNUDSEN <tmphotock@gmail.com>
2018-03-12 16:18:36 -0600

    first commit

---
+++ src/components/app.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1>My Test Application</h1>
+        {console.log('hello mate')}
+      </div>
+    );
+  }
+}
+
